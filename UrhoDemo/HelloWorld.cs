dd5417922342c13704d5f8915d69b3ff270cab55
jucaripo <jucaripo@gmail.com>
2020-06-24 14:06:31 -0500

    * .gitignore: Demo de OpenGL con Xamarin Forms y UrhoSharp
    * UrhoDemo.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * HelloWorld.cs:
    * Info.plist:
    * UrhoDemo.csproj:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * UrhoDemo.iOS.csproj:
    * Default.png:
    * Icon.png:
    * Icon.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * UrhoDemo.Android.csproj:
    * Default-568h@2x.png:
    * Font.ttf:
    * Text.mdl:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * Font.ttf:
    * Apple.mdl:
    * Tabbar.xml:
    * styles.xml:
    * Plane.mdl:
    * Text.mdl:
    * Skull.mdl:
    * colors.xml:
    * Splashscreen.png:
    * AboutResources.txt:
    * Plane.mdl:
    * Apple.mdl:
    * Toolbar.xml:
    * Enemy4.mdl:
    * Splashscreen.png:
    * Skull.mdl:
    * Monkey.mdl:
    * Enemy3.mdl:
    * Player1.mdl:
    * Monkey.mdl:
    * Grass.dds:
    * Default-Portrait@2x.png:
    * Apple.png:
    * LaunchScreen.storyboard:
    * Enemy3.mdl:
    * Enemy4.mdl:
    * Powerup.wav:
    * Monkey.png:
    * Enemy3.png:
    * Player1.mdl:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * Grass.xml:
    * Powerup.wav:
    * Apple.png:
    * Apple.xml:
    * Grass.dds:
    * Resource.designer.cs:
    * demoUrho.mdl:
    * Enemy3.xml:
    * Monkey.png:
    * TreeUV2.png:
    * Enemy3.png:
    * xamarin.png:
    * icon.png:
    * Pyramid.png:
    * Monkey.xml:
    * Grass.xml:
    * Apple.xml:
    * demoUrho.mdl:
    * FXAA3.xml:
    * Player1D.png:
    * Pyramid.png:
    * Enemy3.xml:
    * MachineGun.wav:
    * Enemy4DD.png:
    * icon.png:
    * xamarin.png:
    * Monkey.xml:
    * TreeUV2.png:
    * Enemy4DD.png:
    * Player1D.png:
    * FXAA3.xml:
    * PyramidTree.mdl:
    * Enemy4DD.xml:
    * icon.png:
    * Player1D.xml:
    * MachineGun.wav:
    * Enemy4DD.xml:
    * Explosion.png:
    * Player1D.xml:
    * Anonymous Pro.ttf:
    * Enemy3weapon.mdl:
    * Explosion.pex:
    * BigExplosion.wav:
    * PyramidTree.mdl:
    * Anonymous Pro.ttf:
    * Enemy3weapon.mdl:
    * MachineGun.xml:
    * Explosion.pex:
    * Explosion.png:
    * BigExplosion.wav:
    * MachineGun.xml:
    * icon.xml:
    * SmallExplosion.wav:
    * Enemy3weapon.png:
    * Enemy3weapon.xml:
    * MissileTrace.pex:
    * Enemy3weapon.png:
    * SmallExplosion.wav:
    * MissileTrace.pex:
    * Enemy3weapon.xml:
    * PlayerExplosion.pex:
    * PlayerExplosion.png:
    * PyramidMaterial.xml:
    * Icon58.png:
    * PyramidMaterial.xml:
    * PlayerExplosion.pex:
    * Icon60.png:
    * Icon87.png:
    * PlayerExplosion.png:
    * Icon20.png:
    * Icon29.png:
    * Icon40.png:
    * Icon80.png:
    * Icon76.png:
    * Icon167.png:
    * Icon120.png:
    * Icon152.png:
    * Icon180.png:
    * icon_round.xml:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * HeavyMissileExplosion.png:
    * HeavyMissileExplosion.pex:
    * HeavyMissileExplosion.png:
    * HeavyMissileExplosion.pex:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ UrhoDemo/HelloWorld.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Urho;
+using Urho.Actions;
+using Urho.Gui;
+
+namespace UrhoDemo
+{
+    public class HelloWorld : Urho.Application
+    {
+        public HelloWorld(ApplicationOptions options) : base(new ApplicationOptions(assetsFolder: "Data"))
+        {
+        }
+
+        protected override async void Start()
+        {
+            base.Start();
+            CreateText();
+            await Create3DObject();
+        }
+
+        private void CreateText()
+        {
+            // Create Text Element
+            var text = new Text()
+            {
+                Value = "UrhoSharp \n Comunidad Xamarin en español",
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Bottom
+            };
+
+            text.SetColor(Color.Green);
+            text.SetFont(font: ResourceCache.GetFont("Fonts/Anonymous Pro.ttf"), size: 30);
+            // Add to UI Root
+            UI.Root.AddChild(text);
+        }
+
+        private async Task Create3DObject()
+        {
+            // Scene
+            var scene = new Scene();
+            scene.CreateComponent<Octree>();
+
+            // Node (Rotation and Position)
+            Node node = scene.CreateChild();
+            node.Position = new Vector3(0, 0, 5);
+            node.Rotation = new Quaternion(10, 60, 10);
+            node.SetScale(.5f);
+
+            // Pyramid Model
+            StaticModel modelObject = node.CreateComponent<StaticModel>();
+            //modelObject.Model = ResourceCache.GetModel("Models/Pyramid.mdl");
+            modelObject.Model = ResourceCache.GetModel("Models/Text.mdl");
+            
+            
+
+            // Light
+            Node light = scene.CreateChild(name: "light");
+            light.SetDirection(new Vector3(0.4f, -0.5f, 0.3f));
+            light.CreateComponent<Light>();
+
+            // Camera
+            Node cameraNode = scene.CreateChild(name: "camera");
+            Camera camera = cameraNode.CreateComponent<Camera>();
+
+            // Viewport
+            var viewport = new Viewport(Context, scene, camera, null);
+            Renderer.SetViewport(0, viewport);
+            viewport.SetClearColor( Color.FromHex("#2d96da"));
+
+            // Action
+            await node.RunActionsAsync(
+                new RepeatForever(new RotateBy(duration: 1,
+                    deltaAngleX: 0, deltaAngleY: 190, deltaAngleZ: 0)));
+
+        }
+    }
+}
---
+++ UrhoDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UrhoDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await HelloWorldUrhoSurface.Show<HelloWorld>(new Urho.ApplicationOptions(assetsFolder: "Data"));
+        }
+    }
+}
